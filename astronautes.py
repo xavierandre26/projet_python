80a28882c1041be92a37eed798440f5bc82f259f
André <xavier.andre@univ-rennes2.fr>
2022-03-01 15:02:35 +0100

    Astronautes => CRAFT=ISS

--- astronautes.py
+++ astronautes.py
@@ -1,5 +1,8 @@
 import requests
 import json
 
-astronautes = requests.get("http://api.open-notify.org/astros.json")
-print(astronautes.json())
+astronautes = requests.get("http://api.open-notify.org/astros.json").json()
+print(astronautes)
+for astronaute in astronautes["people"]:
+    if astronaute["craft"] == "ISS":
+        print(astronaute)
